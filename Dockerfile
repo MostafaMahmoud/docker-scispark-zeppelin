b92d4d321a5e483fdeb25557f71a4953d0fd3ea8
Maziyar Boustani <boustani@jpl.nasa.gov>
2016-08-16 16:23:05 -0700

    updated dockerfile and start.sh file for ESIP workshop VM

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,22 @@
-FROM pymonger/scispark-zeppelin:latest
+FROM pymonger/scispark-zeppelin:v0.3_004
+
+
+RUN apt-get update && apt-get install -y automake \
+    autotools-dev \
+    g++ \
+    libcurl4-gnutls-dev \
+    libfuse-dev \
+    libssl-dev \
+    libxml2-dev \
+    make \
+    pkg-config \
+    git \
+    exfat-fuse \
+    exfat-utils \
+    curl \
+    openjdk-7-jdk \
+    maven
+
 
 # Zeppelin
 ENV ZEPPELIN_PORT 8080
@@ -6,10 +24,35 @@ ENV ZEPPELIN_HOME /usr/zeppelin
 ENV ZEPPELIN_CONF_DIR $ZEPPELIN_HOME/conf
 ENV ZEPPELIN_NOTEBOOK_DIR $ZEPPELIN_HOME/notebook
 
-# add startup script
+
+# Mount S3
+RUN echo <put access key for S3 here> > /home/passwd.txt
+RUN chmod 400 /home/passwd.txt
+RUN git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+WORKDIR s3fs-fuse
+RUN ./autogen.sh
+RUN ./configure
+RUN make
+RUN make install
+RUN mkdir -p /mnt/s3
+
+
+#Change the Zeppelin skin
+RUN mkdir -p ~/.m2/repository/org/apache/zeppelin/zeppelin-web/0.6.0-SNAPSHOT/
+RUN cp /usr/zeppelin/zeppelin-web-0.6.0-SNAPSHOT.war ~/.m2/repository/org/apache/zeppelin/zeppelin-web/0.6.0-SNAPSHOT/
+RUN mkdir -p /tmp/zeppelin_skin
+RUN git clone -b zeppelin_0.6.0 https://github.com/SciSpark/scispark_zeppelin_web.git /tmp/zeppelin_skin
+WORKDIR /tmp/zeppelin_skin
+RUN mvn clean install -DskipTests
+RUN cp ./target/zeppelin-web-0.6.0-SNAPSHOT.war /usr/zeppelin/
+
+
+#Add startup script
 COPY start-scispark.sh /root/start-scispark.sh
 RUN set -ex \
  && chmod 755 /root/start-scispark.sh
 
+
+#Start
 WORKDIR /root
 CMD ["./start-scispark.sh"]
--- start-scispark.sh
+++ start-scispark.sh
@@ -1,12 +1,78 @@
 #!/bin/bash
 
-# start sshd for HDFS
-/usr/sbin/sshd
-sleep 10
 
-# start HDFS
+#Copy ESIP workshop notebooks
+rm -rf /usr/zeppelin/notebook/*
+mkdir -p /tmp/notebooks
+git clone https://github.com/SciSpark/scispark_zeppelin_notebooks /tmp/notebooks
+for i in /tmp/notebooks/workshop*/*; do cp -r $i /usr/zeppelin/notebook/; done
+
+
+#Mount S3
+s3fs scispark-workshop /mnt/s3 -o passwd_file=/home/passwd.txt
+
+
+#Start sshd for HDFS
+/usr/sbin/sshd
+sleep 5
+
+
+#Start HDFS
 /usr/hadoop-2.6.3/sbin/start-dfs.sh
 sleep 10
 
-# start Zeppelin
+
+#Remove old data
+hdfs dfsadmin -safemode leave
+hdfs dfs -rmr /data/*
+hdfs dfs -rmr /data
+
+
+#Copy SciSpark JAR file from S3 to local and set the Zeppelin parameters
+mkdir /home/workshop
+cp -r /mnt/s3/scispark_jar /home/workshop
+
+
+#Configure Zeppelin with SciSpark JAR file
+export ZEPPELIN_JAVA_OPTS="-Dspark.jars=/home/workshop/scispark_jar/SciSpark.jar -Dspark.executor.memory=6g"
+export SPARK_SUBMIT_OPTIONS="--jars /home/workshop/scispark_jar/SciSpark.jar --driver-java-options -Xmx20g"
+export ZEPPELIN_WEBSOCKET_MAX_TEXT_MESSAGE_SIZE=1048576
+
+
+#Copy data from S3 to local and Hadoop and local
+hdfs dfs -mkdir /workshop_s3
+hdfs dfs -mkdir /workshop_s3/101
+hdfs dfs -copyFromLocal /mnt/s3/data/101/* /workshop_s3/101
+hdfs dfs -mkdir /workshop_s3/201
+hdfs dfs -mkdir /workshop_s3/201/merg
+hdfs dfs -copyFromLocal /mnt/s3/data/201/merg* /workshop_s3/201/merg
+hdfs dfs -mkdir /workshop_s3/201/trmm
+hdfs dfs -copyFromLocal /mnt/s3/data/201/3B42* /workshop_s3/201/trmm
+hdfs dfs -mkdir /workshop_s3/301
+hdfs dfs -mkdir /workshop_s3/301/pdf_clustering
+hdfs dfs -copyFromLocal /mnt/s3/data/301/pdf_clustering/* /workshop_s3/301/
+
+cp -r /mnt/s3/data/201/viz_data /home/workshop
+cp -r /mnt/s3/data/TRMM_daily /home/workshop
+cp -r /mnt/s3/data/301/pdf_clustering /home/workshop
+cp -r /mnt/s3/data/301/CMDA /home/workshop
+
+
+#Copy and setup anaconda
+cd /usr
+cp -r /mnt/s3/anaconda.tar.gz ./
+tar -xvzf anaconda.tar.gz
+rm -rf anaconda2
+rm -rf anaconda.tar.gz
+mv anaconda anaconda2
+
+
+#Give access to local files
+chmod -R 777 /home/workshop
+
+#Remove passwd.txt file
+rm /home/passwd.txt
+
+
+#Start Zeppelin
 /usr/zeppelin/bin/zeppelin.sh
